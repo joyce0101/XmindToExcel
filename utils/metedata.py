ade0492b0f5bb2a7db36b5bf43ab4c890d5ad056
user <user@userdeMacBook-Pro.local>
2022-07-08 19:43:09 +0800

    add testcase to dict

--- utils/metedata.py
+++ utils/metedata.py
@@ -7,11 +7,26 @@
 @Date    ：2022/6/21 2:23 PM 
 """
 
+config = {
+    'title': '#'
+}
+
 
 class TestCase(object):
-    def __int__(self, title, directory, steps, expectation, priority):
+    def __int__(self, title, directory, steps, expectation, priority, precondition):
         self.title = title
         self.directory = directory
         self.steps = steps
         self.expectation = expectation
         self.priority = priority
+        self.precondition = precondition  # 前置条件
+
+    def to_dict(self):
+        data = {
+            'title': self.title,
+            'directory': self.directory,
+            'steps': self.steps,
+            'expectation': self.expectation,
+            'priority': self.priority,
+            'precondition': self.precondition  # TODO 前置条件可以写在用例标题的笔记里面，暂未实现
+        }
